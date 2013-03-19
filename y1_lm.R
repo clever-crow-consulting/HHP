06516de86fd28980d3b7e27a7533fb3fdab30630
dandye <dye.daniel@gmail.com>
2013-03-19 12:23:18 -0400

    as submitted on 2013-03-17

--- y1_lm.R
+++ y1_lm.R
@@ -1,10 +1,15 @@
-library("ipred")
-library("tree")
-#library("randomForest")
+#library("ipred")
+#library("tree")
+library("randomForest")
 
 y1df <- read.csv("../data/y1_member_claim_count.csv",header=TRUE)
+y1df <- y1df[,1:75]
+y1df <- y1df[complete.cases(y1df),]
+
 #y2df <- read.csv("../data/y2_member_claim_count.csv",header=TRUE)
 ftdf <- read.csv("../data/y3_member_claim_count.csv",header=TRUE)
+ftdf <- ftdf[,1:75]
+ftdf <- ftdf[complete.cases(ftdf),]
 #mem_list <- unique(ftdf$member_id)
 
 #y1df <- y1df[mem_list,]
@@ -12,30 +17,31 @@ ftdf <- read.csv("../data/y3_member_claim_count.csv",header=TRUE)
 
 gc()
 
-y1df[is.na(y1df)] <- 0
-#y2df[is.na(y2df)] <- 0
-ftdf[is.na(ftdf)] <- 0
+#y1df[is.na(y1df)] <- 0  # Gotcha!  This was stupid!
+#y2df[is.na(y2df)] <- 0  # Gotcha!  This was stupid!
+#ftdf[is.na(ftdf)] <- 0  # Gotcha!  This was stupid!
+
+# Predict
 
 #y1lm <- lm(days_in_hospital ~ .-member_id, data=y1df)
 #y2lm <- lm(days_in_hospital ~ .-member_id, data=y2df)
-
 #y1tree <- tree(days_in_hospital ~ .-member_id, data=y1df)
 #y2tree <- tree(days_in_hospital ~ .-member_id, data=y2df)
-y1_treebag <- bagging(days_in_hospital ~ .-member_id, data=y1df)
+#y1_treebag <- bagging(days_in_hospital ~ .-member_id, data=y1df)
 #y2_treebag <- bagging(days_in_hospital ~ .-member_id, data=y2df)
+train_rows = sample(nrow(y1df)/2)
+rf1 <- randomForest(days_in_hospital ~ .-member_id, data=y1df[train_rows,])
 
-gc()
+# Fit
 
 #ftdf$y1lm_fitted_days_in_hospital <- predict(y1tree, ftdf)
 #ftdf$y2lm_fitted_days_in_hospital <- predict(y2tree, ftdf)
-
-ftdf$y1_treebag <- predict(y1_treebag, ftdf)
-gc()
+#ftdf$y1_treebag <- predict(y1_treebag, ftdf)
 #ftdf$y2_treebag <- predict(y2_treebag, ftdf)
-gc()
 #ftdf$avg_treebag <- rowMeans(data.frame(ftdf$y1_treebag,ftdf$y2_treebag))
-gc()
 
+ftdf$rf1 <- predict(rf1, ftdf)
+ 
 #ftdf$avg_fitted_days_in_hospital <- rowMeans(data.frame(ftdf$y1lm_fitted_days_in_hospital,ftdf$y2lm_fitted_days_in_hospital))
 # Remove negative guesses
 #ftdf[ftdf$avg_fitted_days_in_hospital < 0,]$avg_fitted_days_in_hospital <- 0
@@ -43,5 +49,5 @@ gc()
 ftdf$SupLOS <- rep(0,length(ftdf[,1]))
 
 
-write.csv(ftdf[,c("member_id","SupLOS","y1_treebag")], 
-          file="../data/target_20130316_y1treebag.csv", row.names=FALSE)
+write.csv(ftdf[,c("member_id","SupLOS","rf1")], 
+          file="../data/target_20130317_rf1.csv", row.names=FALSE)
