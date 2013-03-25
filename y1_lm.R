0c804bff8273dc0fd747188240ae872912deb12c
dandye <dye.daniel@gmail.com>
2013-03-24 23:40:24 -0400

    clear membory after Y1 and make 500 trees instead of 30

--- y1_lm.R
+++ y1_lm.R
@@ -7,16 +7,8 @@ y1df <- read.csv("../data/y1_member_claim_count.csv",
                  na.strings = c("NA","nan", ""))
                  
 y1df <- y1df[,1:75]
-
-
-
 y1df <- y1df[complete.cases(y1df),]
 
-y2df <- read.csv("../data/y2_member_claim_count.csv",
-                 header=TRUE,
-                 na.strings = c("NA","nan", ""))
-y2df <- y2df[,1:75]
-y2df <- y2df[complete.cases(y2df),]
 
 # Load Prediction
 target <- read.csv("../data/Target.csv",
@@ -38,28 +30,38 @@ for (i in 1:length(y3df$member_id)){
 gc()
 
 # Predict
-
 rf1 <- randomForest(days_in_hospital ~ .-member_id, 
                     data=y1df,
-                    ntree=30, # for debugging
+                    ntree=500, # for debugging
                     sampsize=nrow(y1df)/8,
                     #xtest=subset(y2df, select = -c(member_id,days_in_hospital) ),
                     #ytest=y2df$days_in_hospital,
                     keep.forest=TRUE
                     )
 
+# Predict
+y3df$rf1 <- predict(rf1, y3df )
+
+rm(y1df)
+gc()
+
+print("Y1 done.")
+
+y2df <- read.csv("../data/y2_member_claim_count.csv",
+                 header=TRUE,
+                 na.strings = c("NA","nan", ""))
+y2df <- y2df[,1:75]
+y2df <- y2df[complete.cases(y2df),]
+
 rf2 <- randomForest(days_in_hospital ~ .-member_id, 
                     data=y2df,
-                    ntree=30, # for debugging
+                    ntree=500, # for debugging
                     sampsize=nrow(y1df)/8,
                     #xtest=subset(y3df, select = -c(member_id,days_in_hospital) ),
                     #ytest=y3df$days_in_hospital,
                     keep.forest=TRUE
                     )
 
-# Predict
-
-y3df$rf1 <- predict(rf1, y3df )
 y3df$rf2 <- predict(rf2, y3df )
 
 # Post-process predictions
@@ -70,8 +72,6 @@ y3df$constant <- rep(0.209179,length(y3df[,1]))
 #y3df$avgrf <- rowMeans(data.frame(y3df$rf1,y3df$constant))
 y3df$avgrf <- rowMeans(data.frame(y3df$rf1,y3df$rf2,y3df$constant))
 
-
-
 # Remove negative guesses
 #y3df[y3df$avg_fitted_days_in_hospital < 0,]$avg_fitted_days_in_hospital <- 0
 
@@ -79,7 +79,11 @@ y3df$avgrf <- rowMeans(data.frame(y3df$rf1,y3df$rf2,y3df$constant))
 
 y3df$SupLOS <- rep(0,length(y3df[,1]))
 
-submission_file <- "../submissions/target_20130324.csv"
+#y3df[sapply(y3df[logical_list,]$avgrf,is.na),]$avgrf
+
+y3df[is.na(y3df$avgrf),"avgrf"] <- 0.209179
+
+submission_file <- "../submissions/target_20130325.csv"
 
 write.csv(y3df[logical_list,c("member_id","SupLOS","avgrf")], 
           file=submission_file, row.names=FALSE)
@@ -94,6 +98,7 @@ hist(submission$avgrf)
 plot(density(submission$avgrf))
 print(summary(submission$avgrf))
 
+print(length(submission$avgrf))
 
 
 
